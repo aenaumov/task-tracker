f5e70e4c5c973c7617dd494d8d8382c4a9e5ce6c
Vladimir Ivanov <ivanov.vladimir.l@gmail.com>
2022-04-21 19:45:40 +0300

    4 спринт

--- src/controller/ВПамятиИсторияЗадач.java
+++ src/controller/ВПамятиИсторияЗадач.java
@@ -1,7 +1,9 @@
 package controller;
 
-import java.util.LinkedList;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import model.Задача;
 
@@ -11,16 +13,86 @@ import model.Задача;
  * @author Vladimir Ivanov (ivanov.vladimir.l@gmail.com)
  */
 public class ВПамятиИсторияЗадач implements ИсторияЗадач {
-	LinkedList<Задача> история = new LinkedList<>();
+	Map<Integer, Node<Задача>> nodeByIdMap = new HashMap<>();
+
+	Node<Задача> first;
+	Node<Задача> last;
+	public static class Node<E> {
+		E item;
+		Node<E> next;
+		Node<E> prev;
+
+		Node(Node<E> prev, E element, Node<E> next) {
+			this.item = element;
+			this.next = next;
+			this.prev = prev;
+		}
+	}
+
+	void linkLast(Задача e) {
+		final Node<Задача> последний = last;
+		final Node<Задача> новый = new Node<>(последний, e, null);
+		last = новый;
+		if (последний == null)
+			first = новый;
+		else
+			последний.next = новый;
+		nodeByIdMap.put(e.getИд(), новый);
+	}
 
 	@Override
 	public List<Задача> получитьИсторию() {
-		return история;
+		final List<Задача> list = new ArrayList<>();
+		final Node<Задача> node = first;
+		while (node != null) {
+			// Todo add to list
+		}
+		return list;
 	}
 
 	@Override
 	public void добавить(Задача задача) {
-		история.add(задача);
-		// TODO добавить ограничение на длинну
+		// Todo задача != null
+		final Node<Задача> node = nodeByIdMap.get(задача.getИд());
+		if (node != null) {
+			removeNode(node);
+		}
+		linkLast(задача);
+	}
+
+	private void removeNode(Node<Задача> node) {
+		// 1. Начало
+//		if (node.prev == null)
+		if (node == first) {
+			if (node == last) {
+				first = null;
+				last = null;
+				return;
+			}
+
+			final Node<Задача> newFirst = first.next;
+			newFirst.prev = null;
+			first = newFirst;
+			return;
+		}
+		// 3. Конец
+		if (node == last) {
+			// TODO
+			return;
+		}
+		// 2. Середина
+		// TODO
+		final Node<Задача> prev = node.prev;
+		final Node<Задача> next = node.next;
+		prev.next = next;
+		next.prev = prev;
+	}
+
+	@Override
+	public void удалить(Задача задача) {
+		final Node<Задача> node = nodeByIdMap.remove(задача.getИд());
+		if (node != null) {
+			removeNode(node);
+		}
 	}
 }
